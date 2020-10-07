db96fad22c9dbbfa56d420ac574c5dfc43eff7b6
Marshall Lowry <marshalljacksonlowry@hotmail.com>
2020-10-06 19:17:26 -0500

    second commit

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5600;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, function (req, res) {
+  console.log("App listening on PORT: " + PORT);
+});
